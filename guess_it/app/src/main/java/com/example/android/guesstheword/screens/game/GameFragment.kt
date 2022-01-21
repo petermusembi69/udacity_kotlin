5d9d775bf78cfc13befb85a74f69075657d91955
petermusembi69 <nyongesamusembi@gmail.com>
2022-01-21 17:18:28 +0300

    Add haptic buzzer

--- guess_it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ guess_it/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -16,10 +16,14 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -61,8 +65,30 @@ class GameFragment : Fragment() {
             }
         })
 
+        // Buzzes when triggered with different buzz events
+        viewModel.eventBuzz.observe(viewLifecycleOwner, Observer { buzzType ->
+            if (buzzType != GameViewModel.BuzzType.NO_BUZZ) {
+                buzz(buzzType.pattern)
+                viewModel.onBuzzComplete()
+            }
+        })
+
         return binding.root
 
+
+    }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
     }
 
 }
--- guess_it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ guess_it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -8,25 +8,44 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
+
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
 class GameViewModel : ViewModel(){
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
+    }
 
     companion object {
         // These represent different important times
         // This is when the game is over
         const val DONE = 0L
         // This is the number of milliseconds in a second
+        private const val COUNTDOWN_PANIC_SECONDS = 10L
+
         const val ONE_SECOND = 1000L
         // This is the total time of the game
-        const val COUNTDOWN_TIME = 10000L
+        const val COUNTDOWN_TIME = 30000L
     }
 
     private val timer: CountDownTimer
+
+    private val _eventBuzz = MutableLiveData<BuzzType>()
+    val eventBuzz: LiveData<BuzzType>
+        get() = _eventBuzz
 
     private val _time = MutableLiveData<Long>()
     val time: LiveData<Long>
         get() = _time
 
-    val currentTimeString = Transformations.map(time,{ timeElapsed -> DateUtils.formatElapsedTime(timeElapsed) })
+    val currentTimeString = Transformations.map(time) { timeElapsed -> DateUtils.formatElapsedTime(timeElapsed) }
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
@@ -82,6 +101,7 @@ class GameViewModel : ViewModel(){
 
     fun onCorrect() {
         _score.value = (score.value)?.plus(1)
+        _eventBuzz.value = BuzzType.CORRECT
         nextWord()
     }
 
@@ -95,11 +115,14 @@ class GameViewModel : ViewModel(){
 
             override fun onTick(millisUntilFinished: Long) {
                 _time.value = millisUntilFinished/1000
-            }
+                if (millisUntilFinished / ONE_SECOND <= COUNTDOWN_PANIC_SECONDS) {
+                    _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
+                }
+             }
 
             override fun onFinish() {
                 _eventGameFinish.value = true
-                onGameFinishComplete()
+                _eventBuzz.value = BuzzType.GAME_OVER
             }
 
         }
@@ -114,6 +137,10 @@ class GameViewModel : ViewModel(){
 
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
+    }
+
+    fun onBuzzComplete() {
+        _eventBuzz.value = BuzzType.NO_BUZZ
     }
 }
 
