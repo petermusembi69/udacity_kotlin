d29fdcda3681693b313fe9556e6d32bb6560e1cd
petermusembi69 <nyongesamusembi@gmail.com>
2022-02-03 10:54:25 +0300

    Complete overview screen

--- internet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ internet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -31,17 +31,23 @@ import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
 
+
+enum class MarsApiStatus {LOADING, ERROR, DONE}
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<MarsApiStatus>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
     // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
+    val status: LiveData<MarsApiStatus>
+        get() = _status
 
     private var viewModelJob: Job = Job()
     private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
@@ -60,10 +66,15 @@ class OverviewViewModel : ViewModel() {
         coroutineScope.launch {
             var getPropertiesDeferred = MarsApi.retroFitService.getProperties()
             try {
+                _status.value = MarsApiStatus.LOADING
                 var listResult = getPropertiesDeferred.await()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                _status.value = MarsApiStatus.DONE
+                if (listResult.isNotEmpty()) {
+                    _properties.value = listResult
+                }
             } catch (t: Throwable) {
-                _response.value = "Failure " + t.message
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
             }
         }
     }
