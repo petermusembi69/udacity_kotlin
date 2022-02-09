e6099d6f25921cdacdf54437f92e7797f0369b0b
petermusembi69 <nyongesamusembi@gmail.com>
2022-02-09 14:03:34 +0300

    Finalize app

--- gdg-finder/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ gdg-finder/app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,9 +23,16 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        binding.viewModel = viewModel
 
+        viewModel.navigateToSearch.observe(viewLifecycleOwner, Observer<Boolean> { navigate ->
+            if (navigate) {
+                val navController = findNavController()
+                navController.navigate(R.id.action_homeFragment_to_addGdgFragment)
+            }
+        })
         return view
     }
 }
